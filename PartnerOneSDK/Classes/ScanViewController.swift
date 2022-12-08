0ce26d442237dcc75587d0b8663f8858ddba9ca5
Thyago Casablancas <thyago_casablancas@hotmail.com>
2022-12-08 19:30:28 -0300

    Putting back properties

--- PartnerOneSDK/Classes/ScanViewController.swift
+++ PartnerOneSDK/Classes/ScanViewController.swift
@@ -144,7 +144,7 @@ extension ScanViewController {
     
     
     baseView.cameraContainer.addSubview(baseView.background)
-    baseView.sendSubview(toBack: baseView.cameraContainer)
+    baseView.sendSubviewToBack(baseView.cameraContainer)
   }
 }
 
