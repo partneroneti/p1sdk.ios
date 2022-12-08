be95b51bfbcd4886f48e0af2f0045bcf2353809b
Thyago Casablancas <thyago_casablancas@hotmail.com>
2022-12-08 10:49:55 -0300

    Adjust broken constraints

--- Example/PartnerOneSDK/ScanView.swift
+++ Example/PartnerOneSDK/ScanView.swift
@@ -95,7 +95,7 @@ open class ScanView: BaseView {
     let path = UIBezierPath(roundedRect: background.bounds,
                             byRoundingCorners: [.topLeft, .topRight, .bottomLeft, .bottomRight],
                             cornerRadii: CGSize(width: 20, height: 20))
-    maskLayer.fillRule = kCAFillRuleEvenOdd
+    maskLayer.fillRule = CAShapeLayerFillRule.evenOdd
     
     background.cropReferenceView.addDashedBorder(radius: 10, pattern: [4,4], color: UIColor.white.cgColor)
     background.cropReferenceView.layer.borderWidth = 10
