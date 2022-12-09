c9cd2688779e5d619bc222ab37ca1ce8d1aed5ef
Thyago Casablancas <thyago_casablancas@hotmail.com>
2022-12-08 21:43:30 -0300

    Update Helper

--- Example/PartnerOneSDK/Helper.swift
+++ Example/PartnerOneSDK/Helper.swift
@@ -1,16 +1,56 @@
 import UIKit
+import FaceTecSDK
 
 protocol PartnerHelperProtocol: AnyObject {
+  var mainViewController: UIViewController { get }
   var transactionID: String { get }
 }
 
-public class PartnerHelper: PartnerHelperProtocol {
+open class PartnerHelper: PartnerHelperProtocol {
   
-  var transactionID: String = ""
+  //MARK: - Public Properties
   
-  public func openViewAfter(from viewController: UIViewController, to: UIViewController = UIViewController()) {
-    DispatchQueue.main.async {
-      viewController.navigationController?.pushViewController(to, animated: true)
-    }
+  public var mainViewController = UIViewController()
+  public var transactionID: String = ""
+  
+  //MARK: - init
+
+  public init(mainViewController: UIViewController = UIViewController(),
+              transactionID: String = "") {
+    self.mainViewController = mainViewController
+    self.transactionID = transactionID
+  }
+  
+  //MARK: - Public Functions
+  
+  public func initializeSDK(_ viewController: UIViewController) {
+    let mainWorker = PartnerOneWorker()
+    let mainViewModel = ScanViewModel(worker: mainWorker)
+    let mainViewController = ScanViewController(viewModel: mainViewModel)
+    viewController.navigationController?.pushViewController(mainViewController, animated: true)
+  }
+  
+  public func openViewAfter(_ viewController: UIViewController) {
+    viewController.navigationController?.popToViewController(mainViewController, animated: true)
+  }
+  
+  public func createUserAgentForNewSession() -> String {
+    return ""
+  }
+  
+  public func faceTecDeviceKeyIdentifier(_ clientKey: String = "") -> String {
+    return clientKey
+  }
+  
+  public func faceTecBaseURL(_ url: String = "") -> String {
+    return url
+  }
+  
+  public func faceTecPublicFaceScanEncryptionKey(_ key: String = "") -> String {
+    return key
+  }
+  
+  public func faceTecProductionKeyText(_ key: String = "") -> String {
+    return key
   }
 }
