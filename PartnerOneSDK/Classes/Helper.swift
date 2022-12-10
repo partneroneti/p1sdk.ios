598481e753187c36d002c7c908e8a90de4db910e
Thyago Casablancas <thyago_casablancas@hotmail.com>
2022-12-10 15:06:24 -0300

    Helper navigation

--- PartnerOneSDK/Classes/Helper.swift
+++ PartnerOneSDK/Classes/Helper.swift
@@ -5,9 +5,6 @@ open class PartnerHelper {
   
   //MARK: - Public Properties
   
-  private let mainWorker = PartnerOneWorker()
-  private let mainViewModel = ScanViewModel()
-  
   public var sendDocumentPicture: (() -> Void)?
   public var navigateToStatus: (() -> Void)?
   public var transactionID: String = ""
@@ -21,14 +18,19 @@ open class PartnerHelper {
   //MARK: - Public Functions
   
   public func initializeSDK(_ viewController: UIViewController) {
+    let mainViewModel = ScanViewModel(helper: self)
     viewController.navigationController?.pushViewController(ScanViewController(viewModel: mainViewModel), animated: true)
   }
   
   public func startFaceCapture() -> UIViewController {
+    let mainWorker = PartnerOneWorker()
+    let mainViewModel = ScanViewModel(helper: self)
     return ScanViewController(viewModel: mainViewModel)
   }
   
   public func startDocumentCapture() -> UIViewController {
+    let mainWorker = PartnerOneWorker()
+    let mainViewModel = ScanViewModel(helper: self)
     return FacialScanViewController(viewModel: mainViewModel)
   }
   
@@ -40,7 +42,7 @@ open class PartnerHelper {
     return FaceTec.sdk.createFaceTecAPIUserAgentString("")
   }
   
-  public func createUserAgentForSession(_ sessionToken: String) -> String {
+  public func createUserAgentForSession(_ sessionToken: String = "") -> String {
     return FaceTec.sdk.createFaceTecAPIUserAgentString(sessionToken)
   }
   
