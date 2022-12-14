1fb9358fdcf6f647ff897825520f4e0bd543da6b
Thyago Casablancas <thyago_casablancas@hotmail.com>
2022-12-14 01:18:40 -0300

    update

--- PartnerOneSDK/Classes/DocumentDataModel.swift
+++ PartnerOneSDK/Classes/DocumentDataModel.swift
@@ -1,17 +1,6 @@
 import Foundation
-import ObjectMapper
 
-public struct DocumentDataModel: Mappable {
+public struct DocumentDataModel: Codable {
   var type: String?
   var byte: String?
-  
-  public init?(map: Map) {
-    type = (try? map.value("type")) ?? ""
-    byte = (try? map.value("byte")) ?? ""
-  }
-
-  mutating public func mapping(map: Map) {
-    type <- map["type"]
-    byte <- map["byte"]
-  }
 }
