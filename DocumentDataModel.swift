11c392fbb9e9010a88be5fc6064f041409b04506
Thyago Casablancas <thyago_casablancas@hotmail.com>
2022-12-13 21:56:59 -0300

    Object Mapper insertion

--- DocumentDataModel.swift
+++ DocumentDataModel.swift
@@ -1,6 +1,17 @@
 import Foundation
+import ObjectMapper
 
-public struct DocumentDataModel: Codable {
+public struct DocumentDataModel: Mappable {
   var type: String?
   var byte: String?
+  
+  public init?(map: Map) {
+    type = (try? map.value("type")) ?? ""
+    byte = (try? map.value("byte")) ?? ""
+  }
+
+  mutating public func mapping(map: Map) {
+    type <- map["type"]
+    byte <- map["byte"]
+  }
 }
