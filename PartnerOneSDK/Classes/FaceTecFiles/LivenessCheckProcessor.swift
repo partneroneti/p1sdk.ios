4860f0c8bc26acbd74773dda47955bb670ae7b80
Aller Allegro <aller.allegro@partnergroup.com.br>
2022-12-23 15:20:20 -0300

    ajuste processor

--- PartnerOneSDK/Classes/FaceTecFiles/LivenessCheckProcessor.swift
+++ PartnerOneSDK/Classes/FaceTecFiles/LivenessCheckProcessor.swift
@@ -20,85 +20,94 @@ open class LivenessCheckProcessor: NSObject, Processor, FaceTecFaceScanProcessor
   }
   
   public func processSessionWhileFaceTecSDKWaits(sessionResult: FaceTecSessionResult, faceScanResultCallback: FaceTecFaceScanResultCallback) {
+
+          if sessionResult.status != FaceTecSessionStatus.sessionCompletedSuccessfully {
+            if latestNetworkRequest != nil {
+              latestNetworkRequest.cancel()
+        }
+      
+            faceScanResultCallback.onFaceScanResultCancel()
+            return
+          }
+    
+      self.helper.faceScan=(sessionResult.faceScanBase64 ?? "")
+    self.helper.auditTrailImage=(sessionResult.auditTrailCompressedBase64?[0] ?? "")
+    self.helper.lowQualityAuditTrailImage=(sessionResult.lowQualityAuditTrailCompressedBase64?[0] ?? "")
     
     fromViewController.setLatestSessionResult(sessionResult: sessionResult)
     
     self.faceScanResultCallback = faceScanResultCallback
-    
-    if sessionResult.status != FaceTecSessionStatus.sessionCompletedSuccessfully {
-        if latestNetworkRequest != nil {
-            latestNetworkRequest.cancel()
-        }
-        
-        faceScanResultCallback.onFaceScanResultCancel()
-        return
-    }
-    
-    self.helper.getFaceScan = sessionResult.faceScanBase64 ?? ""
-    
-    self.fromViewController.faceTecLivenessData(faceScanBase: sessionResult.faceScanBase64 ?? "",
-                                                auditTrailImage: sessionResult.auditTrailCompressedBase64?[0] ?? "",
-                                                lowQualityAuditTrailImage: sessionResult.lowQualityAuditTrailCompressedBase64?[0] ?? "") {
-      
-      self.fromViewController.processorResponse?()
-     
-      self.helper.waitingFaceTecResponse = {
-        FaceTecCustomization.setOverrideResultScreenSuccessMessage("Liveness\nConfirmed")
-      }
-    }
+    self.fromViewController.processorResponse?()
     
     print("@! >>> Escaneamento facial feito. Fazendo checagem...")
+      
     
-    var parameters: [String : Any] = [:]
-    parameters["transactionId"] = fromViewController.transactionId
-    parameters["faceScan"] = sessionResult.faceScanBase64
-    parameters["auditTrailImage"] = sessionResult.auditTrailCompressedBase64![0]
-    parameters["lowQualityAuditTrailImage"] = sessionResult.lowQualityAuditTrailCompressedBase64![0]
-    parameters["sessionId"] = fromViewController.sessionId
-    parameters["deviceKey"] = fromViewController.deviceKey
+//    if sessionResult.status != FaceTecSessionStatus.sessionCompletedSuccessfully {
+//      if latestNetworkRequest != nil {
+//        latestNetworkRequest.cancel()
+//      }
+//
+//      faceScanResultCallback.onFaceScanResultCancel()
+//      return
+//    }
+
     
-    var request = URLRequest(url: NSURL(string: "https://integracao-sodexo-homologacao.partner1.com.br/api/liveness")! as URL)
-    request.httpMethod = "POST"
-    request.addValue("application/json", forHTTPHeaderField: "Content-Type")
-    request.httpBody = try! JSONSerialization.data(withJSONObject: parameters, options: JSONSerialization.WritingOptions(rawValue: 0))
-//    request.addValue("dALwVmFSvUCNkBq7v3wf5Fk8oiivfsfM", forHTTPHeaderField: "X-Device-Key")
-//    request.addValue(helper.createUserAgentForSession(), forHTTPHeaderField: "User-Agent")
+//    let BaseURL = "https://digital-id.webdatadome.com/api"
+//    var parameters: [String : Any] = [:]
+//    parameters["transactionId"] = Config.TransactionID
+//    parameters["faceScan"] = sessionResult.faceScanBase64
+//    parameters["auditTrailImage"] = sessionResult.auditTrailCompressedBase64![0]
+//    parameters["lowQualityAuditTrailImage"] = sessionResult.lowQualityAuditTrailCompressedBase64![0]
+//    parameters["sessionId"] = helper.createUserAgentForSession()
+//    parameters["deviceKey"] = "dioxhbQHfJHmnHTzJ40hCiUm3FR0IkXY"
+//
+//    var request = URLRequest(url: NSURL(string: Config.BaseURL + "/liveness")! as URL)
+//    request.httpMethod = "POST"
+//    request.addValue("application/json", forHTTPHeaderField: "Content-Type")
+//    request.httpBody = try! JSONSerialization.data(withJSONObject: parameters, options: JSONSerialization.WritingOptions(rawValue: 0))
+//
+//
+//    let session = URLSession(configuration: URLSessionConfiguration.default, delegate: self, delegateQueue: OperationQueue.main)
+//    latestNetworkRequest = session.dataTask(with: request as URLRequest, completionHandler: { data, response, error in
+//
+//      guard let data = data else {
+//        faceScanResultCallback.onFaceScanResultCancel()
+//        return
+//      }
+//
+//      guard let responseJSON = try? JSONSerialization.jsonObject(with: data, options: JSONSerialization.ReadingOptions.allowFragments) as! [String: AnyObject] else {
+//        faceScanResultCallback.onFaceScanResultCancel()
+//        return
+//      }
+//
+//      guard let scanResultBlob = responseJSON["scanResultBlob"] as? String,
+//            let wasProcessed = responseJSON["wasProcessed"] as? Bool else {
+//        faceScanResultCallback.onFaceScanResultCancel()
+//        return;
+//      }
+//
+//      if wasProcessed == true {
+//        FaceTecCustomization.setOverrideResultScreenSuccessMessage("Liveness\nConfirmed")
+//        self.success = faceScanResultCallback.onFaceScanGoToNextStep(scanResultBlob: scanResultBlob)
+//      } else {
+//        faceScanResultCallback.onFaceScanResultCancel()
+//        return;
+//      }
+//    })
+//
+//    latestNetworkRequest.resume()
+//
+//    DispatchQueue.main.asyncAfter(deadline: .now() + 6) {
+//      if self.latestNetworkRequest.state == .completed { return }
+//      let uploadMessage: NSMutableAttributedString = NSMutableAttributedString.init(string: "Still Uploading...")
+//      faceScanResultCallback.onFaceScanUploadMessageOverride(uploadMessageOverride: uploadMessage)
+//    }
     
-    let session = URLSession(configuration: URLSessionConfiguration.default, delegate: self, delegateQueue: OperationQueue.main)
-    latestNetworkRequest = session.dataTask(with: request as URLRequest, completionHandler: { data, response, error in
-        
-        guard let data = data else {
-            faceScanResultCallback.onFaceScanResultCancel()
-            return
-        }
-        
-      guard let responseJSON = try? JSONSerialization.jsonObject(with: data, options: JSONSerialization.ReadingOptions.allowFragments) as? [String: AnyObject] else {
-//            faceScanResultCallback.onFaceScanResultCancel()
-            return
-        }
-        
-        guard let scanResultBlob = responseJSON?["scanResultBlob"] as? String,
-              let wasProcessed = responseJSON?["wasProcessed"] as? Bool else {
-            faceScanResultCallback.onFaceScanResultCancel()
-            return;
-        }
-        
-        if wasProcessed == true {
-            FaceTecCustomization.setOverrideResultScreenSuccessMessage("Liveness\nConfirmed")
-            self.success = faceScanResultCallback.onFaceScanGoToNextStep(scanResultBlob: scanResultBlob)
-        } else {
-            faceScanResultCallback.onFaceScanResultCancel()
-            return;
-        }
-    })
-    
-    latestNetworkRequest.resume()
-    
-    DispatchQueue.main.asyncAfter(deadline: .now() + 6) {
-        if self.latestNetworkRequest.state == .completed { return }
-        let uploadMessage: NSMutableAttributedString = NSMutableAttributedString.init(string: "Still Uploading...")
-        faceScanResultCallback.onFaceScanUploadMessageOverride(uploadMessageOverride: uploadMessage)
-    }
+      PartnerHelper.livenessCallBack!(sessionResult.faceScanBase64 ?? "" ,
+                                sessionResult.auditTrailCompressedBase64?[0] ?? "", sessionResult.lowQualityAuditTrailCompressedBase64?[0] ?? "")
+      faceScanResultCallback.onFaceScanResultCancel()
+      
+      
   }
   
   public func urlSession(_ session: URLSession, task: URLSessionTask, didSendBodyData bytesSent: Int64, totalBytesSent: Int64, totalBytesExpectedToSend: Int64) {
